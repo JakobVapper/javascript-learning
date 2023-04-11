a391d448140a24a5485c523f05cfd883619b9b61
JakobVapper <jakob.vapper@ametikool.ee>
2023-04-11 11:01:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Variables
+
+const myName = "Jakob Vapper"; // String
+let myAge = 17; // Number
+const myChildren = ["Jakob jr", "Jakob jr2"];
+const canRideBicycle = true; // Boolean
+const person = {
+    myName: "Jakob Vapper",
+    myAge: 17,
+}; // Object
+
+console.log(myName, myAge);
+console.log(myChildren);
+console.log(canRideBicycle);
+console.log(person);
